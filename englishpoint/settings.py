d63c522f6ba2823198ac2540eae34b3743be4ac4
matix0508 <matix0508@gmail.com>
2020-06-13 02:07:43 +0200

    init

--- englishpoint/settings.py
+++ englishpoint/settings.py
@@ -124,4 +124,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = ( os.path.join('organizer/static'), )
+STATICFILES_DIRS = ( os.path.join('handler/static'), )
